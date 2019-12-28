e7607ba1e57cac832baec0a12fc05c773b8d494f
Ronit Chattopadhyay <ronit.chattopadhyay@quikr.com>
2019-12-28 17:38:25 +0530

    Chapter 39: Using react transitions

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition'
+
 import '../css/App.css';
 
 class TransitionComp extends Component{
 
+    state = {
+        show: false
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true: false
+        })
+    }
+
     render(){
         return(
             <div>
-                Transition
+                <Transition
+                    in={this.state.show} 
+                    timeout={2000} 
+                    mountOnEnter 
+                    unmountOnExit
+                >
+                    { state => 
+                        <div style={{
+                            background: 'grey',
+                            height:'100px',
+                            transition:'all 2s ease',
+                            opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+                        }}>
+                            {state}                            
+                        </div>
+                    }
+                </Transition>
+                <div className='showDiv' onClick={this.showDiv}>Show or Hide</div>
             </div>
-
         )
     }
 }
