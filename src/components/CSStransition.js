cf0afbf3b09c392e8cbf550aed7d2412d0801087
Ronit Chattopadhyay <ronit.chattopadhyay@quikr.com>
2020-01-14 08:46:29 +0530

    Chapter 41 [Legacy] CSS transitions

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import '../css/App.css';
 
 class Fade extends Component{
+
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true: false
+        })
+    }
+
     render(){
         return(
             <div>
-                CSS TRANSITIONS
+                <CSSTransition 
+                    in={this.state.show} 
+                    timeout={5000} 
+                    classNames="square"
+                >
+                    <div className={`square ${this.state.show}`}>Hello</div>
+                </CSSTransition>
+                <div className="showDiv" 
+                     onClick={this.showDiv}
+                >
+                    Show or Hide
+                </div>
             </div>
         )
     }
