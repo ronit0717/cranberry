7e558dff6902f151898b0fdf16b247efb6ec19ee
Ronit Chattopadhyay <ronit.chattopadhyay@quikr.com>
2020-01-15 10:59:23 +0530

    Chapter 41: [Legacy] CSS Transitions

--- src/components/tgroup.js
+++ src/components/tgroup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import '../css/App.css';
 
 class Slide extends Component{
@@ -8,7 +9,16 @@ class Slide extends Component{
     
     addElements () {
         return this.state.items.map((item,i) => (
-            <div className="item" key={i}>{item}</div>
+            <CSSTransition 
+                classNames="item"
+                timeout={500}
+                key={i}
+                onEntered = { (node) => {
+                    node.classList.add('active')
+                }}
+            >
+                <div className="item" key={i}>{item}</div>
+            </CSSTransition>
         ));
     }
 
@@ -31,7 +41,11 @@ class Slide extends Component{
     render(){
         return(
             <div>
-                {this.addElements()}
+                <TransitionGroup 
+                component="div"
+                className="list">
+                    {this.addElements()}
+                </TransitionGroup>
              
                 <div className="btns">
                     <div className="btn-add" onClick={()=> this.generateNumber()}>Add Elements</div>
